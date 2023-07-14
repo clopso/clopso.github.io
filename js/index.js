d94603038b63d8a2dc3c608830b82d5b9ddad7bb
clopso <caiolemes306@gmail.com>
2023-07-14 15:31:46 -0300

    mobile responsive

---
+++ js/index.js
@@ -0,0 +1,24 @@
+document.addEventListener('keydown', function(e) {
+    console.log(e)
+    switch (e.key) {
+        case "PageUp":
+            jump("header");
+            break;
+        case "ArrowUp":
+            jump("header");
+            break;
+
+        case "PageDown":
+            jump("talk");
+            break;
+        case "ArrowDown":
+            jump("talk");
+            break;
+    }
+});
+
+function jump(h){
+    var url = location.href;               //Save down the URL without hash.
+    location.href = "#"+h;                 //Go to the target element.
+    history.replaceState(null,null,url);   //Don't like hashes. Changing it back.
+}
